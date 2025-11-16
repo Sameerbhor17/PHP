cde4727d9d02cfd20ef2b8216c00107728a29091
Sameer Bhor <115771003+Sameerbhor17@users.noreply.github.com>
2025-11-16 06:34:38 +0000

    Variables and Data Types in PHP

---
+++ PHP-PROGRAMS/VariablesAndDataTypes.php
@@ -0,0 +1,18 @@
+<?php
+
+$text = "Sameer"; // PHP String Data
+echo($text);
+echo "<br/>";
+
+$number = 15; //PHP Integer Data
+echo $number;
+echo "<br/>";
+
+$boolean_data = true; //PHP Boolean Data - true or false
+echo $boolean_data;
+echo "<br/>";
+
+$arr = [10, 20, "PHP", 8.2, true, "PHP-V.8.2.18", false];
+echo $arr;
+
+?>
