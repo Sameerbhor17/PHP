9734b25716df4cdf2abeba95bf3fbef2900aba0e
Sameer Bhor <sameerbhor859@gmail.com>
2025-11-17 07:58:19 +0530

    Prime Number code corrections

--- PHP-PROGRAMS/PrimeNumber.php
+++ PHP-PROGRAMS/PrimeNumber.php
@@ -3,13 +3,19 @@
 //Write a code to check if a number is prime or not
 
 $number = 11;
+$isPrime = false;
 
 for($i=2; $i<=($number/2); $i++) {
-    if(($number/$i) === 0) {
+    if(($number/i) === 0) {
         echo "$number is not a prime number";
+        break;
     } else {
-        echo "$number is a prime number";
+        $isPrime = true;
     }
+}
+
+if($isPrime) {
+    echo "$number is a Prime Number";
 }
 
 ?>
