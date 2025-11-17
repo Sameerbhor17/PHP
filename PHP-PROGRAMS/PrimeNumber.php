72ca98ee02fa4b1c38e01fc0b3ef0c9e34502a33
Sameer Bhor <sameerbhor859@gmail.com>
2025-11-17 08:25:14 +0530

    Prime Number in PHP

--- PHP-PROGRAMS/PrimeNumber.php
+++ PHP-PROGRAMS/PrimeNumber.php
@@ -1,21 +1,23 @@
 <?php
 
-//Write a code to check if a number is prime or not
+$number = 12;
+$isPrime = true;
 
-$number = 11;
-$isPrime = false;
-
-for($i=2; $i<=($number/2); $i++) {
-    if(($number/$i) === 0) {
-        echo "$number is not a prime number";
-        break;
-    } else {
-        $isPrime = true;
+if ($number <= 1) {
+    $isPrime = false;
+} else {
+    for ($i = 2; $i <= $number / 2; $i++) {
+        if ($number % $i === 0) {
+            $isPrime = false;
+            break;
+        }
     }
 }
 
-if($isPrime) {
+if ($isPrime) {
     echo "$number is a Prime Number";
+} else {
+    echo "$number is NOT a Prime Number";
 }
 
 ?>
