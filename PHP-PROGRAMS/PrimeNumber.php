882badc83dd092731970fd6a18c5f83cc0856b31
Sameer Bhor <115771003+Sameerbhor17@users.noreply.github.com>
2025-11-17 02:19:50 +0000

    Pime Number Program in PHP

---
+++ PHP-PROGRAMS/PrimeNumber.php
@@ -0,0 +1,15 @@
+<?php
+
+//Write a code to check if a number is prime or not
+
+$number = 11;
+
+for($i=2; i<=($number/2); $i++) {
+    if(($number/i) === 0) {
+        echo "$number is not a prime number";
+    } else {
+        echo "$number is a prime number";
+    }
+}
+
+?>
