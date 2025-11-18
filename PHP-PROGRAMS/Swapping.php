494c059c4ebcfa84b5e6188ddfc4ec3fe7a81bfc
Sameer Bhor <115771003+Sameerbhor17@users.noreply.github.com>
2025-11-18 02:55:30 +0000

    adding new line for variable swapping logic

--- PHP-PROGRAMS/Swapping.php
+++ PHP-PROGRAMS/Swapping.php
@@ -3,7 +3,7 @@
 $a = 20;
 $b = 50;
 
-echo "Numbers before swapping a=$a and b=$b";
+echo "<b>Numbers before swapping a=$a and b=$b</b> <br/>";
 
 function swap($a, $b) {
     //addition and substraction
@@ -11,7 +11,7 @@ function swap($a, $b) {
     $b = $a - $b;
     $a = $a - $b;
 
-    echo "Numbers after swapping a=$a and b=$b";
+    echo "<b>Numbers after swapping a=$a and b=$b</b>";
 }
 
 swap(20, 50);
