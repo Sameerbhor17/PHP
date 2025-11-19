eb122a6f616a690700a9889c4204c02f5434466c
Sameer Bhor <115771003+Sameerbhor17@users.noreply.github.com>
2025-11-19 03:19:10 +0000

    Reverse Rotated Star Pattern

--- PHP-PROGRAMS/StarPattern.php
+++ PHP-PROGRAMS/StarPattern.php
@@ -1,21 +1,33 @@
 <?php
 
-$n = 5;
+    $n = 5;
 
-echo "<b>Star Triangle Pattern</b> <br/>";
-for($i = 1; $i <= $n; $i++) {
-    for($j = 1; $j <= $i; $j++) {
-        echo "*";
+    echo "<b>Star Pattern</b> <br/>";
+    for($i = 1; $i <= $n; $i++) {
+        for($j = 1; $j <= $i; $j++) {
+            echo "*";
+        }
+        echo "<br/>";
     }
-    echo "<br/>";
-}
 
-echo "<br/>";
-echo "<b>Rotated Star Triangle Pattern</b> <br/>";
-for($i = $n; $i >= 1; $i--) {
-    for($j = 1; $j <= $i; $j++) {
-        echo "*";
-    }
     echo "<br/>";
-}
+    echo "<b>Rotated Star Pattern</b> <br/>";
+    for($i = $n; $i >= 1; $i--) {
+        for($j = 1; $j <= $i; $j++) {
+            echo "*";
+        }
+        echo "<br/>";
+    }
+
+    echo "<br/>";
+    echo "<b>Reverse Rotated Star Pattern</b> <br/>";
+    for($i = $n; $i >= 1; $i--) {
+        for($j = 1; $j <= $n-$i; $j++) {
+            echo " ";
+        }
+        for($j = 1; $j <= $i; $j++) {
+            echo "*";
+        }
+        echo "<br/>";
+    }
 ?>
