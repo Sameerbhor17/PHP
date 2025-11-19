19c0f9e43786a36c0b5bc15111d1a3306a3eaf2b
Sameer Bhor <sameerbhor859@gmail.com>
2025-11-19 08:27:07 +0530

    Added printArr function to print array elements before and after sorting

--- PHP-PROGRAMS/BubbleSort.php
+++ PHP-PROGRAMS/BubbleSort.php
@@ -1,6 +1,8 @@
 <?php
 
 $arr = [4, 8, 1, 5, 2, 6, 3, 9, 7, 0];
+echo "<b> Array Elements before Sorting: </b> <br/>";
+printArr($arr);
 
 for($i = 0; $i < count($arr)-1; $i++) {
     for($j = 0; $j < count($arr)-$i-1; $j++) {
@@ -12,8 +14,14 @@ for($i = 0; $i < count($arr)-1; $i++) {
     }
 }
 
-foreach($arr as $el) {
-    echo $el." ";
+print("<br/><br/>");
+echo "<b> Array Elements After Sorting using Bubble Sort: </b> <br/>";
+printArr($arr);
+
+function printArr($array) {
+    foreach($array as $el) {
+        echo $el." ";
+    }
 }
 
 ?>
