495f673633c885c844282dcef0c1e92e1f8f86a6
Sameer Bhor <115771003+Sameerbhor17@users.noreply.github.com>
2025-11-19 02:47:24 +0000

    Bubble Sort syntax error for $j variable fix

--- PHP-PROGRAMS/BubbleSort.php
+++ PHP-PROGRAMS/BubbleSort.php
@@ -4,10 +4,10 @@ $arr = [4, 8, 1, 5, 2, 6, 3, 9, 7, 0];
 
 for($i = 0; $i < count($arr)-1; $i++) {
     for($j = 0; $j < count($arr)-$i-1; $j++) {
-        if($arr[j] > $arr[j+1]) {
-            $temp = $arr[j];
-            $arr[j] = $arr[j+1];
-            $arr[j+1] = $temp;
+        if($arr[$j] > $arr[$j+1]) {
+            $temp = $arr[$j];
+            $arr[$j] = $arr[$j+1];
+            $arr[$j+1] = $temp;
         }
     }
 }
