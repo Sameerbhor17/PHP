b4eef1b5bdb74ad9f71fae9911fb1d4d3775c58a
Sameer Bhor <115771003+Sameerbhor17@users.noreply.github.com>
2025-11-20 03:17:51 +0000

    added extra line new-line using br tag

--- PHP-PROGRAMS/RectanglePatterns.php
+++ PHP-PROGRAMS/RectanglePatterns.php
@@ -4,6 +4,7 @@
 
     //Rectangle Pattern
     echo "<b>Rectangle Pattern</b>";
+    echo "<br/>";
     for($i = 1; $i <= $n; $i++) {
         for($j = 1; $j <= $n; $j++) {
             echo "*";
@@ -16,6 +17,7 @@
 
     //Hollow Rectangle Pattern
     echo "<b>Hollow Rectangle Pattern</b>";
+    echo "<br/>";
     for($i = 1; $i <= $n; $i++) {
         for($j = 1; $j <= $n; $j++) {
             if($i === 1 || $j === 1 || $i === $n || $j === $n) {
