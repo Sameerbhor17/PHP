a6b74daf04852ef6ddd9c38f7e558b9291c47eed
Sameer Bhor <115771003+Sameerbhor17@users.noreply.github.com>
2025-11-20 03:16:24 +0000

    Print Hollow Rectangle Pattern in PHP

--- PHP-PROGRAMS/RectanglePatterns.php
+++ PHP-PROGRAMS/RectanglePatterns.php
@@ -1,13 +1,29 @@
 <?php 
 
-$n = 5;
+    $n = 5;
 
-//Rectangle Pattern
-for($i = 1; $i <= $n; $i++) {
-    for($j = 1; $j <= $n; $j++) {
-        echo "*";
+    //Rectangle Pattern
+    echo "<b>Rectangle Pattern</b>";
+    for($i = 1; $i <= $n; $i++) {
+        for($j = 1; $j <= $n; $j++) {
+            echo "*";
+        }
+        echo "<br/>";
     }
-    echo "<br/>";
-}
 
+    echo "<br/>";
+    echo "<br/>";
+
+    //Hollow Rectangle Pattern
+    echo "<b>Hollow Rectangle Pattern</b>";
+    for($i = 1; $i <= $n; $i++) {
+        for($j = 1; $j <= $n; $j++) {
+            if($i === 1 || $j === 1 || $i === $n || $j === $n) {
+                echo "*";
+            } else {
+                echo "&nbsp";
+            }
+        }
+        echo "<br/>";
+    }
 ?>
