73de8d0b6df8f2c89a5539a204e36be607fe231a
Sameer Bhor <115771003+Sameerbhor17@users.noreply.github.com>
2025-11-22 02:46:47 +0000

    Number Palindrome Pattern in PHP

---
+++ PHP-PROGRAMS/PalindromePattern.php
@@ -0,0 +1,24 @@
+<?php 
+
+    $n = 5;
+
+    //Palindrome Pattern
+    echo "<b>Palindrome Pattern<b><br/>";
+    echo "<br/>";
+
+    for($i = 1; $i <= $n; $i++) {
+        for($j = 1; $j <= ($n - $i); $j++) {
+            echo "&nbsp;&nbsp;";
+        }
+
+        for($j = 1; $j <= $i; $j++) {
+            echo "$j";
+        }
+
+        for($j = $i; $j >= 1; $j--) {
+            echo "$j";
+        }
+        echo "<br/>";
+    }
+
+?>
